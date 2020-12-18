c68191c19c9f41eb3e87a6b2e76731641bf380fb
zui88 <mburkhardt6@gmail.com>
2020-12-18 14:57:42 +0100

    add doom emacs

---
+++ .doom.d/custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" default))
+ '(org-agenda-files
+   '("~/studi/algo1/algorithmen.org" "~/studi/betriebsSys/betriebSys.org" "~/studi/mecha-projekt/mecha-projekt.org" "~/studi/rechnerArchi/rechnerarchitektur.org" "~/studi/schedule.org" "~/org/tasks.org"))
+ '(org-directory "~/study"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-variable 'disabled nil)
